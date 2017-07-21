ded9242b75b8c03ca708188fa8bc0579370c57c7
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-07-22 00:31:44 +0900

    As of 201707220031

--- gitcommitwip.sh
+++ gitcommitwip.sh
@@ -2,7 +2,7 @@
  
 ##Commit commands for backup
 git checkout wip 
-git add bibliography.bib body.tex
+git add bibliography.bib body.tex paper.tex gitcommitwip.sh
 git commit -m "As of $(date +"%Y%m%d%H%M")"
 git push
 
