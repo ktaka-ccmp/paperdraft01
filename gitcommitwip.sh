1a233db3570f1e32cb83085951fae2db45a3882b
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-08-05 12:34:00 +0900

    As of 201708051234

--- gitcommitwip.sh
+++ gitcommitwip.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 branch=kt
-git show-branch $branch || git brabch $branch
- 
+if git show-branch $branch ; then
+	git checkout $branch
+else
+	git checkout -b $branch
+	git push --set-upstream origin
+fi
+
 ##Commit commands for backup
-git checkout $branch
 git add bibliography.bib body.tex paper.tex gitcommitwip.sh
 git add Figs
 git commit -m "As of $(date +"%Y%m%d%H%M")"
